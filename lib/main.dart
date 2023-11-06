cfc7f03e29bd7261b9007a409a6e50f4fe40f5fa
gilmujjang <alsanrlf@naver.com>
2023-11-06 22:01:40 +0900

    feat: Pomodors 화면 디자인
    화면 작업

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/screen/wallet.dart';
+import 'package:toonflix/screens/homeScreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Wallet();
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xffe7626c),
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xff232b55),
+          ),
+        ),
+        cardColor: const Color(0xfff4eddb),
+      ),
+      home: const Scaffold(
+        body: HomeScreen(),
+      ),
+    );
   }
 }
